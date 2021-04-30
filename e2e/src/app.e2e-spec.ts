c708b493ba3707b42bb7812195ed69565837081a
thendokhae <thendo.makahane@itq.co.za>
2021-04-30 12:59:46 +0200

    updated name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to slider-menu!');
+    expect(page.getTitleText()).toEqual('Welcome to scroll-menu-app!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'slider-menu'`, () => {
+  it(`should have as title 'scroll-menu-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('slider-menu');
+    expect(app.title).toEqual('scroll-menu-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to slider-menu!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to scroll-menu-app!');
   });
 });
